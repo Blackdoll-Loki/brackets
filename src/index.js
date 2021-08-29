e8311f68f95d8fac3cdf86bd9644e8f03abd90a3
Blackdoll-Loki <helpmeloki7@gmail.com>
2021-08-29 17:51:30 +0300

    init: start brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openBrackets = [];
+  let bracketsPair = {};
+  let stack =[];
+  for(let i = 0; i < bracketsConfig.length; i++){
+      openBrackets[i] = bracketsConfig[i][0];
+      bracketsPair[bracketsConfig[i][1]] = bracketsConfig[i][0];
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    let currentBracket = str[i];
+
+    if(openBrackets.includes(currentBracket)){
+      stack.push(currentBracket);
+    } else {
+        if (stack.length === 0){
+            return false;
+        }
+        
+        let topElement = stack[stack.length - 1];
+
+        if (bracketsPair[currentBracket] === topElement){
+            stack.pop();
+        } else {
+            return false;
+        }
+    }
+  }
+
+  return stack.length === 0;
 }
